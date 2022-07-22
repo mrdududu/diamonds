35447c615c51d3c2f54563282cabacca9902b128
Semen Ishinov <ectoplazm@gmail.com>
2022-07-22 12:20:52 +0300

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,7 +32,26 @@ div(class="container mx-auto grid grid-cols-1 lg:grid-cols-12 lg:gap-x-16" style
     div(class="py-10")
       h2 Цвет
       p(class="mt-8 text-xl") Главный фактор, влияющий на оценку #[br] ювелирного цветного камня.
-  slot
+  div(class="lg:col-start-1 lg:col-span-12")
+    slot
+  div(class="lg:col-start-1 lg:col-span-12")
+    div
+      img(src="/img/photos/diamonds.jpg" class="block w-full")
+    div(class="px-14")
+      div(class="py-16")
+        h2 О компании
+        div(class="mt-8 text-xl space-y-5")
+          p MIUZ diamonds - одно из крупнейших предприятий в мире, которое обеспечивает закрытый цикл производства от добычи алмаза до изготовления ювелирного изделия.
+          p Сегодня  мы сосредоточены на производстве ювелирных украшений с бриллиантами, продаже сертифицированных бриллиантов  от 0.70 ct, которые можно приобрести  по биржевой цене, а также на изготовлении украшений по индивидуальному заказу.
+          p Уже более 100 лет MIUZ diamonds выступает гарантом качества и надежности собственной продукции
+      div(class="pb-24 grid grid-cols-1 lg:grid-cols-3")
+        div
+          img(src="/img/miuz_logo.svg")
+        div(class="lg:col-span-2 pt-9")
+          div(class="mb-14 text-3xl") MIUZ diamonds - официальный партнер Тинькофф, #[br] один из мировых лидеров в ювелирной отрасли
+          div(class="grid grid-cols-1 lg:grid-cols-3")
+            div
+              tfButton Каталог
 </template>
 <script>
 import tfButton from '~/components/uikit/tfButton.vue';
