9db013bd3676cefde6fefe64fbd831f71e45e95c
Semen Ishinov <ectoplazm@gmail.com>
2022-07-27 14:25:54 +0300

    tfTabMenu script setup

--- components/uikit/tfTabMenu.vue
+++ components/uikit/tfTabMenu.vue
@@ -1,12 +1,9 @@
 <template lang="pug">
 div(class="flex")
-  tfTabBtn(v-for="tab in tabs" @click="$emit('tabClick', tab)" :key="tab.key" :selected="tab.selected") {{tab.text}}
+  UikitTfTabBtn(v-for="tab in tabs" @click="$emit('tabClick', tab)" :key="tab.key" :selected="tab.selected") {{tab.text}}
 </template>
-<script>
-import tfTabBtn from '~/components/uikit/tfTabBtn.vue';
-export default {
-  props: ['tabs'],
-  components: { tfTabBtn },
-  setup() {},
-};
+<script setup>
+const props = defineProps({
+  tabs: Object,
+});
 </script>
