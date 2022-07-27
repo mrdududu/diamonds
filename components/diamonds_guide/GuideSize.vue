402915d63e567de6b480b1eff29116f67c8c7a8e
Semen Ishinov <ectoplazm@gmail.com>
2022-07-27 10:55:38 +0300

    Add Guide sizes

---
+++ components/diamonds_guide/GuideSize.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+GuideLayout
+  template(#title) Карат
+  template(#text) Масса камня в каратах #[br] (1 карат равен 0,2 грамма).
+  template(#default)
+    div(v-for="{shape, sizes} in sizesData" :key="shape.key" class="mb-16")
+      div(class="mb-2 text-xs text-center") огранка
+      div(class="mb-8 text-xl text-center text-tf-yellow") {{shape.name}}
+      div(class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8")
+        div(v-for="size in sizes" class="text-xs text-center")
+          div(class="h-20 flex justify-center items-end")
+            img(:src="`/img/diamonds/shape/shape_${shape.key}.svg`" style="width: 46px")
+          div {{size.ct}} карат
+          div {{formatSizeString(size)}}
+</template>
+<script>
+import GuideLayout from '~/components/diamonds_guide/GuideLayout.vue';
+import sizesData from '~/components/diamonds_guide/data/size.json';
+export default {
+  components: {GuideLayout},
+  setup() {
+    // console.log({sizesData});
+
+    return {
+      sizesData,
+      formatSizeString(size){
+        if (size.mm){
+          return `${size.mm} мм`
+        }
+        else if (size.hmm && size.wmm) {
+          return `${size.hmm}х${size.wmm} мм`
+        }
+
+        return '';
+      }
+    };
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,19 @@ div(class="px-14")
     tfTabBtn Чистота
     tfTabBtn Огранка
     tfTabBtn Карат
-  GuideColors
-  GuideClarity
-  GuideShape
+  //- GuideColors
+  //- GuideClarity
+  //- GuideShape
+  GuideSize
 </template>
 <script>
 import tfTabBtn from '~/components/uikit/tfTabBtn.vue';
 import GuideColors from '~/components/diamonds_guide/GuideColors.vue';
 import GuideClarity from '~/components/diamonds_guide/GuideClarity.vue';
 import GuideShape from '~/components/diamonds_guide/GuideShape.vue';
+import GuideSize from '~/components/diamonds_guide/GuideSize.vue';
 export default {
-  components: {tfTabBtn, GuideColors, GuideClarity, GuideShape},
+  components: {tfTabBtn, GuideColors, GuideClarity, GuideShape, GuideSize},
   setup() {
     
   },
