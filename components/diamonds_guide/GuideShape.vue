076d2750b6b0668d7e645668651dd27afad5191a
Semen Ishinov <ectoplazm@gmail.com>
2022-07-27 08:43:37 +0300

    Guide Shape complete

---
+++ components/diamonds_guide/GuideShape.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+GuideLayout
+  template(#title) Огранка
+  template(#text) Учитываются форма, тип огранки, пропорции, #[br] качество обработки (симметрия, полировка и т. д.).
+  template(#default)
+    div(class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-16")
+      div(v-for="item in gridData" :key="item.key")
+        div(class="h-52 flex items-center")
+          img(:src="`/img/diamonds/${imgKey}/${imgKey}_${item.key}.svg`")
+        div(class="text-xl mb-4") {{item.name}}
+        div(class="text-sm") {{item.desc}}
+</template>
+<script>
+import GuideLayout from '~/components/diamonds_guide/GuideLayout.vue';
+import gridData from '~/components/diamonds_guide/data/shape.json';
+export default {
+  components: {GuideLayout},
+  setup() {
+    // console.log({gridData});
+
+    return {imgKey: 'shape', gridData};
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,15 @@ div(class="px-14")
     tfTabBtn Карат
   GuideColors
   GuideClarity
+  GuideShape
 </template>
 <script>
 import tfTabBtn from '~/components/uikit/tfTabBtn.vue';
 import GuideColors from '~/components/diamonds_guide/GuideColors.vue';
 import GuideClarity from '~/components/diamonds_guide/GuideClarity.vue';
+import GuideShape from '~/components/diamonds_guide/GuideShape.vue';
 export default {
-  components: {tfTabBtn, GuideColors, GuideClarity},
+  components: {tfTabBtn, GuideColors, GuideClarity, GuideShape},
   setup() {
     
   },
