bd2e9d185e13d194bfd10bb714cdab2868405a90
Semen Ishinov <ectoplazm@gmail.com>
2022-07-28 09:12:34 +0300

    add square image place

--- components/catalog/Preview.vue
+++ components/catalog/Preview.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 div(class="bg-tf-gray-dark rounded-xl")
-  div
-  div(class="pa-8")
+  div(class="bg-tf-white rounded-xl square") wec
+  div(class="p-3 text-xs")
     div Бриллиант
-    div огранка Круглая
+    div.text-sm.py-2 огранка Круглая
     div Вес: 0.92 карата
     div Граней: 57
     div Цвет: 4
