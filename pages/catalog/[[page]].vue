f7c0bd5d7080202ec2e7b056129a6d929c9c01ce
Semen Ishinov <ectoplazm@gmail.com>
2022-07-28 16:42:06 +0300

    Add page router navigation

--- pages/catalog/[[page]].vue
+++ pages/catalog/[[page]].vue
@@ -5,23 +5,30 @@ div(class="")
   div(class="grid grid-cols-2 md:grid-cols-4 gap-12")
     CatalogPreview(v-for="item in items" :key="item")
   div.my-16.flex.justify-center
-    CatalogPageNav(:totalPages="state.totalPages" :pageIndex="state.pageIndex" :indent="1" @page-click="pageClick")
+    CatalogPageNav(:totalPages="state.totalPages" :pageIndex="pIndex" :indent="1" @page-click="pageClick")
 </template>
 <script setup>
 import { reactive } from 'vue';
 
 const route = useRoute();
+const router = useRouter();
 
-console.log('Route index', { pageIndex: Number(route.params.pageIndex) });
+console.log('Route page', { pageIndex: Number(route.params.page) });
+
+const pIndex = computed(() => {
+  let page = Number(route.params.page);
+  page = page ? page : 1;
+  return page - 1;
+});
 
 const state = reactive({
   totalPages: 12,
-  pageIndex: 0,
 });
 
 const items = Array.from(Array(12), (_, x) => x);
 const pageClick = (pageIndex) => {
   console.log('pageClick', pageIndex);
-  state.pageIndex = pageIndex;
+  const url = pageIndex ? `/catalog/${pageIndex + 1}` : '/catalog';
+  router.push(url);
 };
 </script>
