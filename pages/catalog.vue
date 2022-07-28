6461d922f0a3ab84aff56e2058a1d6ace0002650
Semen Ishinov <ectoplazm@gmail.com>
2022-07-28 15:54:00 +0300

    add page state

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -5,11 +5,19 @@ div(class="")
   div(class="grid grid-cols-2 md:grid-cols-4 gap-12")
     CatalogPreview(v-for="item in items" :key="item")
   div.my-16.flex.justify-center
-    CatalogPageNav(:totalPages="12" :pageIndex="3" :indent="1" @page-click="pageClick")
+    CatalogPageNav(:totalPages="state.totalPages" :pageIndex="state.pageIndex" :indent="1" @page-click="pageClick")
 </template>
 <script setup>
+import { reactive } from 'vue';
+
+const state = reactive({
+  totalPages: 12,
+  pageIndex: 0,
+});
+
 const items = Array.from(Array(12), (_, x) => x);
 const pageClick = (pageIndex) => {
   console.log('pageClick', pageIndex);
+  state.pageIndex = pageIndex;
 };
 </script>
