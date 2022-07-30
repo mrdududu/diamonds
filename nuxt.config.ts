b0daec84b57aa4b5124448349f532dcb45f151e5
Semen Ishinov <ectoplazm@gmail.com>
2022-07-30 11:29:49 +0300

    Add link to doc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // https://v3.nuxtjs.org/guide/features/runtime-config
   runtimeConfig: {
     public: {
       apiHost: 'https://bbavbtoga8rvputnh66l.containers.yandexcloud.net',
