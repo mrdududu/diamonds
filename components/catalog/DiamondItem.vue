f7254146ee20189914b33673a6877fc29e995fdf
Semen Ishinov <ectoplazm@gmail.com>
2022-08-01 15:15:30 +0300

    Add DiamondItem component

---
+++ components/catalog/DiamondItem.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+  div(class="bg-tf-white rounded-xl square")
+  div(class="p-3 text-sm")
+    div Бриллиант
+    div.text-base.py-2 огранка Круглая
+    div Вес: {{item.dia_carat}} карата
+    div Граней: 57
+    div Цвет: {{item.dia_color}}
+    div Чистота: 8
+    div Тип огранки: A
+    div Цена: {{item.dia_price_tink}} руб
+</template>
+<script setup>
+const props = defineProps({
+  item: Object,
+});
+</script>
--- components/catalog/Preview.vue
+++ components/catalog/Preview.vue
@@ -1,15 +1,5 @@
 <template lang="pug">
-div(class="bg-tf-gray-dark rounded-xl")
-  div(class="bg-tf-white rounded-xl square") wec
-  div(class="p-3 text-sm")
-    div Бриллиант
-    div.text-base.py-2 огранка Круглая
-    div Вес: {{item.dia_carat}} карата
-    div Граней: 57
-    div Цвет: {{item.dia_color}}
-    div Чистота: 8
-    div Тип огранки: A
-    div Цена: {{item.dia_price_tink}} руб
+CatalogDiamondItem(class="bg-tf-gray-dark rounded-xl" :item="item")
 </template>
 <script setup>
 const props = defineProps({
