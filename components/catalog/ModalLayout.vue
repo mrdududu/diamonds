7faa5f571357ad7569fac35ee1e99276d9f8c3c2
Semen Ishinov <ectoplazm@gmail.com>
2022-08-02 16:39:36 +0300

    fix css design

--- components/catalog/ModalLayout.vue
+++ components/catalog/ModalLayout.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div(class="fixed inset-0 bg-black bg-opacity-20 overflow-y-auto h-full w-full" @click.self="emit('closeClick')")
-  div(class="relative top-20 mx-auto p-5 max-w-screen-lg shadow-lg rounded-md bg-tf-gray-dark" :class="class")
+  div(class="relative top-20 mx-auto p-16 max-w-screen-lg shadow-lg rounded-md bg-tf-gray-dark" :class="class")
     div.flex.flex-row-reverse(v-if="showClose")
       img.cursor-pointer(src="/img/icons/x.svg" @click="emit('closeClick')")
     slot
