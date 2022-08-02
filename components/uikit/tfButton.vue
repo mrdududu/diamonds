9008de64167844b3620fbdc00baae70aab52f9be
Semen Ishinov <ectoplazm@gmail.com>
2022-08-02 11:06:59 +0300

    Fix Button cursor

--- components/uikit/tfButton.vue
+++ components/uikit/tfButton.vue
@@ -1,4 +1,4 @@
 <template lang="pug">
-NuxtLink(class="block bg-tf-yellow hover:bg-tf-yellow-hover active:bg-tf-yellow-push text-tf-black text-sm py-4 text-center rounded-xl transition-color duration-200")
+NuxtLink(class="block bg-tf-yellow cursor-pointer hover:bg-tf-yellow-hover active:bg-tf-yellow-push text-tf-black text-sm py-4 text-center rounded-xl transition-color duration-200")
   slot
 </template>
