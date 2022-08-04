535fd8dacc79faef368a413348a4ecb95fb0c0ff
Semen Ishinov <ectoplazm@gmail.com>
2022-08-04 08:45:34 +0300

    Tab buttons fix

--- components/uikit/tfTabBtn.vue
+++ components/uikit/tfTabBtn.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(class="block text-xl mr-10 border-b-2 border-transparent pb-1.5 cursor-pointer" :class="{'border-tf-yellow': selected}")
+div(class="block text-xl mr-10 border-b-2 pb-1.5 cursor-pointer" :class="{'border-tf-yellow': selected, 'border-transparent': !selected}")
   slot
 </template>
 <script setup>
