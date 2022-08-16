30226caaa881b1b5702082292a6a1f07202bd0a7
Semen Ishinov <ectoplazm@gmail.com>
2022-08-16 15:33:08 +0300

    Product card make responsive

--- components/best/ProductCard.vue
+++ components/best/ProductCard.vue
@@ -1,21 +1,20 @@
 <template lang="pug">
-div.flex
-  div(class="flex justify-around items-start rounded-xl px-20 py-12" style="background-color: #444444;")
+div(class="md:max-w-xl grid grid-cols-2 gap-x-5 md:gap-x-10 py-4 md:py-12 px-6 md:px-20 md:rounded-xl" style="background-color: #444444;")
+  div
     div
-      div(class="mr-8" style="width: 186px;")
-        img(src="/img/catalog/round.png")
-      CatalogHighProfit(percent="63" text="Высокая динамика доходности")    
-    div(class="ml-8 text-sm")
-      div Бриллиант
-      div.text-base.py-2 огранка Круглая
-      div Вес: {{item.dia_carat}} карата
-      div Граней: 57
-      div Цвет: {{item.dia_color}}
-      div Чистота: 8
-      div Тип огранки: A
-      div Цена: {{item.dia_price_tink}} руб
-      div(class="mt-6" style="width: 180px;")
-        UikitTfButtonAccent Оставить заявку
+      img(src="/img/catalog/round.png")
+    CatalogHighProfit(percent="63" text="Высокая динамика доходности")    
+  div(class="text-sm")
+    div Бриллиант
+    div.text-base.py-2 огранка Круглая
+    div Вес: {{item.dia_carat}} карата
+    div Граней: 57
+    div Цвет: {{item.dia_color}}
+    div Чистота: 8
+    div Тип огранки: A
+    div Цена: {{item.dia_price_tink}} руб
+    div(class="mt-6")
+      UikitTfButtonAccent Оставить заявку
 </template>
 <script setup>
 const props = defineProps({
