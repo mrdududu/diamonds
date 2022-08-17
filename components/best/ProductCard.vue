edd59cc55e1c0ba0efaf3e7e88af6bb0ae2060df
Semen Ishinov <ectoplazm@gmail.com>
2022-08-17 12:51:58 +0300

    Fix bg gradient

--- components/best/ProductCard.vue
+++ components/best/ProductCard.vue
@@ -1,5 +1,10 @@
+<style scoped>
+.product_card {
+  background: linear-gradient(253.46deg, #313131 0%, #1e1e1e 100%);
+}
+</style>
 <template lang="pug">
-div(class="md:max-w-xl grid grid-cols-2 gap-x-5 md:gap-x-10 py-4 md:py-12 px-6 md:px-20 md:rounded-xl" style="background-color: #444444;")
+div(class="md:max-w-xl grid grid-cols-2 gap-x-5 md:gap-x-10 py-4 md:py-12 px-6 md:px-20 md:rounded-xl product_card")
   div
     div
       img(src="/img/catalog/round.png")
